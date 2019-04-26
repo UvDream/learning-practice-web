97efe157146f23f3fd65a2fc9fd23e1ed3ca4bd5
wangzhongjie <wangzhongjie@njxnet.com>
2019-04-26 16:55:10 +0800

    脚本完成

--- other/main.go
+++ other/main.go
@@ -7,64 +7,56 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
-	"path/filepath"
 	"runtime"
 	"strings"
 )
 
 func main() {
-
-	fmt.Println(CurrentFile())
-
-	//fmt.Println(getCurrentDirectory())
-
-	//readFile()
+	fmt.Println("该脚本版权所属来修帮!请勿随意传播使用!")
+	fmt.Println("启用该脚本前请先确认需要修改的ip地址和新的ip地址,如有乱输后果自负!")
+	//读取当前的文件夹地址
+	filePath := CurrentFile()
+	//读取文件
+	fileNameArray, _ := readPathFile(filePath, ".js")
+	replaceFile(fileNameArray, filePath)
 }
 
-//获取程序运行目录
-func getCurrentDirectory() string {
-	dir, _ := filepath.Abs(filepath.Dir(os.Args[0])) //返回绝对路径  filepath.Dir(os.Args[0])去除最后一个元素的路径
-
-	return strings.Replace(dir, "\\", "/", -1)
-}
-
-//读取当前文件路径
+// 读取当前文件路径
 func CurrentFile() string {
 	_, file, _, ok := runtime.Caller(1)
 	if !ok {
-		panic(errors.New("Can not get current file info"))
+		panic(errors.New("无法获取当前目录"))
 	}
-	fmt.Println(path.Ext(file))
+	//打印文件类型
+	//fmt.Println(path.Ext(file))
 	a := strings.Replace(file, "main.go", "", 100)
-	fmt.Println(a)
-	return file
+	return a
+}
+func readPathFile(dirPth string, suffix string) (files []string, err error) {
+	files = make([]string, 0, 10)
+
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+
+	PthSep := string(os.PathSeparator)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			continue
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
+			files = append(files, dirPth+PthSep+fi.Name())
+		}
+	}
+
+	return files, nil
 }
 
-//读取文件
-func readFile() {
-	file, err := os.Open("E:/code/learning-practice-web/other/test.js")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer file.Close()
-	fileinfo, err := file.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fileSize := fileinfo.Size()
-	buffer := make([]byte, fileSize)
-
-	bytesread, err := file.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("字节数:", bytesread)
-	//fmt.Println("文件内容:", string(buffer))
-
+// 匹配文件替换
+func replaceFile(fileNameArray []string, dirPth string) {
 	//获取旧ip
 	oldVal := bufio.NewReader(os.Stdin)
 	fmt.Print("请输入需要替换的ip:")
@@ -73,8 +65,26 @@ func readFile() {
 	newVal := bufio.NewReader(os.Stdin)
 	fmt.Print("请输入更改的ip地址:")
 	newData, _, _ := newVal.ReadLine()
+	for _, a := range fileNameArray {
+		file, err := os.Open(a)
+		dirPth = dirPth + a
+		defer file.Close()
+		fileinfo, err := file.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fileSize := fileinfo.Size()
+		buffer := make([]byte, fileSize)
 
-	content := strings.Replace(string(buffer), string(oldData), string(newData), bytesread)
-	fmt.Println(content)
-	ioutil.WriteFile("E:/code/learning-practice-web/other/test.js", []byte(content), os.ModeAppend)
+		bytesread, err := file.Read(buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println("字节数:", bytesread)
+		//fmt.Println("文件内容:", string(buffer))
+		content := strings.Replace(string(buffer), string(oldData), string(newData), bytesread)
+		if ioutil.WriteFile(a, []byte(content), os.ModeAppend) == nil {
+			fmt.Println("写入文件成功!!!")
+		}
+	}
 }
