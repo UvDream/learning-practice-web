3a6e903c8511b385ef989c6bb53231f02d6fb5b5
wangzhongjie <uvdream@163.com>
2019-05-05 14:47:12 +0800

    数组根据某一字段排序

---
+++ es6/array/index.js
@@ -0,0 +1,18 @@
+var arr = [{
+        id: 1,
+        read: true,
+        name: "第一组"
+    },
+    {
+        id: 2,
+        read: false,
+        name: "第二组"
+    },
+    {
+        id: 3,
+        read: true,
+        name: "第三组"
+    }]
+    console.log('排序前',this.arr)
+    this.arr.sort((a, b) => b.read - a.read)
+    console.log('排序后',this.arr)
