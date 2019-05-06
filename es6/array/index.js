868a7db5bbf493520d0bed40e68795a1e9f3f1f4
wangzhongjie <uvdream@163.com>
2019-05-06 09:46:32 +0800

    数组方法

--- es6/array/index.js
+++ es6/array/index.js
@@ -18,6 +18,27 @@ var arr = [
         name: "第三组"
     }
 ]
-console.log('排序前', this.arr)
+console.log('排序前', arr)
 this.arr.sort((a, b) => b.read - a.read)
-console.log('排序后', this.arr)
+console.log('排序后', arr)
+
+/**
+ * 合并两个数组
+ */
+var a=["迈凯伦"]
+var b=["篮球"]
+var c =["足球"]
+var d=a.concat(b,c)
+console.log("合并数组",d)
+/**
+ * 将数组变成字符串
+ */
+var e=d.join(d)
+console.log("数组变字符串",e)
+
+/**
+ * 删除数组最后一个元素
+ */
+var f=["1","2","3"]
+console.log("删除最后一个的元素",f.pop())
+console.log("删除后的数组",f)
