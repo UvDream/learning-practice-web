f6f9557195338b41beadfa64eed6290e73407370
wangzhongjie <uvdream@163.com>
2019-05-06 16:13:38 +0800

    格式化

--- es6/array/from.js
+++ es6/array/from.js
@@ -4,22 +4,22 @@ console.log("%c-----------ES6 Array.from()语法探究-----------------", "color
  * 类对象转换为真数组
  * 或者扩展运算符
  */
-let arrayLike={
-    "0":"a",
-    "1":"b",
-    "2":"c",
-    length:3
+let arrayLike = {
+    "0": "a",
+    "1": "b",
+    "2": "c",
+    length: 3
 }
 //ES5写法
 console.log("ES5写法", [].slice.call(arrayLike))
 //ES6写法
-console.log("ES6写法",Array.from(arrayLike))
+console.log("ES6写法", Array.from(arrayLike))
 //扩展运算符
 // 实际应用
-let ps =document.querySelectorAll('p')
+let ps = document.querySelectorAll('p')
 // 获取所有的p,这个ps就是一个伪数组
 console.log(ps)
-Array.from(ps).forEach(p=>{
+Array.from(ps).forEach(p => {
     console.log(p)
 })
 
@@ -34,35 +34,38 @@ function add() {
     }
     return sum;
 }
-function add1(){
-    [...arguments].forEach(element=>{
+
+function add1() {
+    [...arguments].forEach(element => {
         console.log(element)
     })
 }
-console.log("arguments",add(1,2,3))
+console.log("arguments", add(1, 2, 3))
 // 扩展字符伪数组转真数组
 add1(1, 2, 3)
 
 // Array.from()第二个参数,类似于数组map方法,对数组每个数据处理,处理后的值返回数组
-let arr1={
-    "0":"1",
-    "1":"2",
-    "2":"3",
-    length:3
+let arr1 = {
+    "0": "1",
+    "1": "2",
+    "2": "3",
+    length: 3
 }
 console.log(Array.from(arr1))
 console.log("第二个参数", Array.from(arr1, x => x * x))
 // 等同于map
 console.log("map实现", Array.from(arr1).map(x => x * x))
-let spans=document.querySelectorAll('span')
-console.log("获取span NodeList",spans)
+let spans = document.querySelectorAll('span')
+console.log("获取span NodeList", spans)
 // map
-let name1=Array.prototype.map.call(spans,s=>s.textContent)
-console.log("map方法",name1)
+let name1 = Array.prototype.map.call(spans, s => s.textContent)
+console.log("map方法", name1)
 // Array.from()
-let name2=Array.from(spans,s=>s.textContent)
-console.log("Array.from()",name2)
+let name2 = Array.from(spans, s => s.textContent)
+console.log("Array.from()", name2)
 
-console.log("特别使用",Array.from({length:2},()=>"jack"))
+console.log("特别使用", Array.from({
+    length: 2
+}, () => "jack"))
 
 // 可以测字符串长度
