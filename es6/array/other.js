6cd9b128193679da16b3f89eae796f9a292b6166
wangzhongjie <uvdream@163.com>
2019-05-06 17:06:38 +0800

    find

--- es6/array/other.js
+++ es6/array/other.js
@@ -24,4 +24,18 @@ console.log([1, 2, 3, 4, 5].copyWithin(0, 3, 4)) // [4, 2, 3, 4, 5]
 // -2相当于3号位置,-1相当于5好位置
 console.log([1, 2, 3, 4, 5].copyWithin(0, -2, -1)) // [4, 2, 3, 4, 5]
 
-
+console.log("%c-----------ES6 数组实例的find()/findIndex()语法探究-----------------", "color: orange;font-size:20px");
+/**
+ * find()用于找出第一个符合条件的数组成员,参数为回调函数
+ * 回调函数三个值
+ * value:数值
+ * index:序号
+ * arr:原数组
+ */
+// 找出第一个小于0的成员
+console.log("find():找出小于0的成员",[1,-4,10].find((x)=>x<0))
+let find=[1,5,10,12].find(function(value,index,arr){
+    console.log(value,index,arr)
+    return value>9
+})
+console.log(find)
