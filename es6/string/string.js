cbe5998f6168fd6a038ab55f859eaa9693f336b1
wangzhongjie <uvdream@163.com>
2019-05-07 11:39:23 +0800

    charAt

---
+++ es6/string/string.js
@@ -0,0 +1,6 @@
+console.log("%c-----------ES5 String语法探究-----------------", "color: red;font-size:20px");
+/**
+ * charAt() 方法可返回指定位置的字符
+ */
+var str = "HELLO WORLD";
+console.log("charat():", str.charAt(2))
