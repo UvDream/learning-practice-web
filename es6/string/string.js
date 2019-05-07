c0042717b9ea794ad370f4a4b879eecb9789d1b4
wangzhongjie <uvdream@163.com>
2019-05-07 17:17:18 +0800

    字符串方法

--- es6/string/string.js
+++ es6/string/string.js
@@ -41,3 +41,27 @@ console.log("search():",str.search("HELLO"))
  * slice() 提取字符串的某个部分， 并以新的字符串返回被提取的部分
  */
 console.log("slice():",str.slice(1,3))
+/**
+ * split() 把一个字符串分割成字符串数组
+ */
+console.log("split():",str.split(""))
+/**
+ * substr() 抽取指定数目的字符
+ */
+console.log("substr():", str.substr(2,3))
+/**
+ * substring() 方法用于提取字符串中介于两个指定下标之间的字符
+ * 两个参数:
+ * from(必需): 一个非负的整数， 规定要提取的子串的第一个字符在 string Object 中的位置
+ * to(可选): 个非负的整数， 比要提取的子串的最后一个字符在 string Object 中的位置多 1,如果省略该参数,那么返回的子串会一直到字符串的结尾
+ */
+console.log(str.substring(3) + "<br>")
+/**
+ * toLowerCase() 把字符串转换为小写
+ * toUpperCase() 把字符串转换为大写
+ */
+console.log("toLowerCase():",str.toLowerCase())
+/**
+ * valueOf() String 对象的原始值
+ */
+console.log("valueOf()", str.valueOf())
