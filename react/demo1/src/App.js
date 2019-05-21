99d0552cf07dee4dbbe037dc67ccf92259b19ee9
wangzhongjie <uvdream@163.com>
2019-05-21 20:41:01 +0800

    react入门

--- react/demo1/src/App.js
+++ react/demo1/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+// import React from 'react';
+// const Component=React.Component;
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        Hello World!
+      </div>
+    )
+  }
 }
-
 export default App;
