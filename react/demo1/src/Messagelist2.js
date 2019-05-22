555d1baacb810ff9f7ab43acb3405ef4c4e27553
wangzhongjie <uvdream@163.com>
2019-05-22 17:47:07 +0800

    context传递值

---
+++ react/demo1/src/Messagelist2.js
@@ -0,0 +1,55 @@
+import React,{Component} from 'react'
+import PropTypes from 'prop-types';
+// 按钮
+function Button(props,context) {
+    return ( 
+        <button style = {
+            {
+                background: context.color
+            }
+        } > {
+            props.children
+        } </button>
+    )
+}
+Button.propTypes = {
+    children: PropTypes.string.isRequired
+}
+Button.contextTypes={
+    color:PropTypes.string.isRequired
+}
+// 消息
+function Message(props) {
+    return ( 
+        <li> {props.text} <Button> 删除</Button> </li>
+    )
+}
+Message.propType = {
+    text: PropTypes.string.isRequired,
+}
+class MessageList extends Component {
+    getChildContext(){
+        return {color:"orange"}
+    }
+    render(){
+        const messages=[
+             {text: "Hello"}, 
+             {text: "React"}
+        ];
+        const children=messages.map((message,key)=>
+        <Message key={key} text={message.text} />
+        )
+        return(
+            <div>
+                <p>
+                    通过Context将color传递给里面的Button组件
+                </p>
+                <ul>{children}</ul>
+            </div>
+        )
+    }
+}
+MessageList.childContextTypes={
+    color:PropTypes.string.isRequired
+}
+export default MessageList;
--- react/demo1/src/index.js
+++ react/demo1/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import Counter from "./Counter"
-import MessageList from "./Messagelist1";
+import MessageList from "./Messagelist2";
 
 // ReactDOM.render( <Counter  / > , document.getElementById('root'));
-ReactDOM.render( < MessageList / > , document.getElementById('root')); 
+ReactDOM.render( < MessageList /> , document.getElementById('root')); 
