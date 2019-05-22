edbd4bd65249c647c9c196683d096dc4e96deab3
wangzhongjie <uvdream@163.com>
2019-05-22 17:54:02 +0800

    组件划分

--- react/demo1/src/App.js
+++ react/demo1/src/App.js
@@ -2,11 +2,23 @@ import React,{Component} from 'react';
 // import React from 'react';
 // const Component=React.Component;
 import './App.css';
+import Counter from "./Counter"
+import MessageList1 from "./Messagelist1";
+import MessageList2 from "./Messagelist2";
+
 class App extends Component {
   render(){
     return(
-      <div>
-        Hello World!
+      <div className="main">
+        <section>
+          < Counter />
+        </section>
+        < section >
+        <MessageList1 />
+        </section>
+        <section >
+          < MessageList2 />
+        </section>
       </div>
     )
   }
--- react/demo1/src/index.js
+++ react/demo1/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import Counter from "./Counter"
-import MessageList from "./Messagelist2";
+import App from './App';
 
-// ReactDOM.render( <Counter  / > , document.getElementById('root'));
-ReactDOM.render( < MessageList /> , document.getElementById('root')); 
+ReactDOM.render( < App /> , document.getElementById('root')); 
+
