817985c1a03b1056a86c6fe2bac8f096ccbc8745
wangzhongjie <uvdream@163.com>
2019-06-25 17:31:58 +0800

    快速配置

---
+++ Rollup/Test1/build/rollup.config.js
@@ -0,0 +1,15 @@
+const path=require('path');
+const buble=require('rollup-plugin-buble');
+const resolve=function(filePath){
+    return path.join(__dirname,'..',filePath)
+}
+module.exports={
+    input:resolve('src/index.js'),
+    output:{
+        file:resolve('dist/index.js'),
+        format:'iife'
+    },
+    plugin:[
+        buble()
+    ]
+}
