a02671bb09ab0d4682614679709f8623dc81181f
wangzhongjie <uvdream@163.com>
2019-07-01 13:55:37 +0800

    依赖收集Dep

--- vue-source-code/vue-analysis/2.3收集依赖/index.js
+++ vue-source-code/vue-analysis/2.3收集依赖/index.js
@@ -1,22 +1,25 @@
+import Dep from "./deep";
 
 // defineProperty简单封装
 function defineReactive(data,key,val){
     // 收集依赖
-    let dep=[]
+    let dep=new Dep()
     Object.defineProperty(data,key,{
         enumerable:true,
         configurable:true,
         get:function(){
-            deep.push(window.target)
+            // deep.push(window.target)
+            dep.depend()
             return val
         },
         set:function(newVal){
             if(val === newVal){
                 return
             }
-            for(let i=0;i<dep.length;i++){
-                dep[i](newVal,val)
-            }
+            // for(let i=0;i<dep.length;i++){
+            //     dep[i](newVal,val)
+            // }
+            dep.notify()
             val=newVal
         }
     })
