0a61d59b69bae59f1bacc3c6c6e8f1d61623f304
wangzhongjie <uvdream@163.com>
2019-08-01 22:56:21 +0800

    [feat] 动画

--- react/demo1/src/App.js
+++ react/demo1/src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
         <section>
           <Animate />
         </section>
+      
       </div>
     )
   }
--- react/demo1/src/lenarn/animate.jsx
+++ react/demo1/src/lenarn/animate.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './style.css'
-
+import './style.css';
+import { CSSTransition } from 'react-transition-group'
 class Animate extends Component {
     constructor(props) {
         super(props);
@@ -12,6 +12,14 @@ class Animate extends Component {
     render() { 
         return (  
             <div>
+                <CSSTransition
+                    in={this.state.isShow}   //用于判断是否出现的状态
+                    timeout={2000}           //动画持续时间
+                    classNames="boss-text"   //className值，防止重复
+                    unmountOnExit  //删除dom
+                >
+            <div>呵呵呵</div>
+                </CSSTransition>
                 <div className={this.state.isShow?'show':'hide'}>这是标题</div>
                 <div>
                     <button onClick={this.toToggole} >点击</button>
