e8ed6034468be19cbd0f5ecaff8c203759eeec2e
wangzhongjie <uvdream@163.com>
2019-08-06 21:00:36 +0800

    [feat] antdesign

---
+++ react/demo4/src/pages/ToDoList.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from 'antd/es/button';
+
+class ToDoList extends Component {
+    render() { 
+        return ( 
+            <div>
+                Hello Redux!
+                <Button type="primary">Button</Button>
+            </div>
+            
+         );
+    }
+}
+ 
+export default ToDoList;
