0ee4273699e6868dc68ed6cb1ca00816066d6eb3
wangzhongjie <uvdream@163.com>
2019-08-06 21:41:23 +0800

    [feat] redux

--- react/demo4/src/pages/ToDoList.jsx
+++ react/demo4/src/pages/ToDoList.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import { Input, Button,List } from 'antd';
-const data = [
-    '早上八点开会',
-    '晚上八点下班'
-]
+import { Input, Button, List } from 'antd';
+import store from '../store';
+// const data = [
+//     '早上八点开会',
+//     '晚上八点下班'
+// ]
 class ToDoList extends Component {
+    constructor(props) {
+        super(props)
+        console.log(store.getState())
+        this.state=store.getState()
+    }
     render() { 
         return ( 
             <div>
                 <div style={{margin:'10px'}}>
-                    <Input placeholder="请输入" style={{ width: '250px',marginRight:'20px' }} />
+                    <Input placeholder={this.state.inputValue} style={{ width: '250px',marginRight:'20px' }} />
                     <Button  type="primary" >新增</Button>
                 </div>
                 <div style={{margin:'10px',width:'300px'}}>
-                    <List bordered dataSource={data} renderItem={item => (<List.Item>{item}</List.Item>)} />
+                    <List bordered dataSource={this.state.list} renderItem={item => (<List.Item>{item}</List.Item>)} />
                 </div>
             </div> 
          );
