6e296951cc87c43aa98e81b6130c315f69fdf732
wangzhongjie <uvdream@163.com>
2019-08-06 21:11:52 +0800

    [feat] antd

--- react/demo4/src/pages/ToDoList.jsx
+++ react/demo4/src/pages/ToDoList.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import Button from 'antd/es/button';
-
+import { Input, Button,List } from 'antd';
+const data = [
+    '早上八点开会',
+    '晚上八点下班'
+]
 class ToDoList extends Component {
     render() { 
         return ( 
             <div>
-                Hello Redux!
-                <Button type="primary">Button</Button>
-            </div>
-            
+                <div style={{margin:'10px'}}>
+                    <Input placeholder="请输入" style={{ width: '250px',marginRight:'20px' }} />
+                    <Button  type="primary" >新增</Button>
+                </div>
+                <div style={{margin:'10px',width:'300px'}}>
+                    <List bordered dataSource={data} renderItem={item => (<List.Item>{item}</List.Item>)} />
+                </div>
+            </div> 
          );
     }
 }
