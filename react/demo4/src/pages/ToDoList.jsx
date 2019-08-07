594e764f8ed6710c52a2495610d4ba4845e84029
wangzhongjie <uvdream@163.com>
2019-08-07 11:18:53 +0800

    [fix] 输入框redux

--- react/demo4/src/pages/ToDoList.jsx
+++ react/demo4/src/pages/ToDoList.jsx
@@ -1,29 +1,51 @@
-import React, { Component } from 'react';
-import { Input, Button, List } from 'antd';
-import store from '../store';
+import React, { Component } from "react";
+import { Input, Button, List } from "antd";
+import store from "../store";
 // const data = [
 //     '早上八点开会',
 //     '晚上八点下班'
 // ]
 class ToDoList extends Component {
-    constructor(props) {
-        super(props)
-        console.log(store.getState())
-        this.state=store.getState()
-    }
-    render() { 
-        return ( 
-            <div>
-                <div style={{margin:'10px'}}>
-                    <Input placeholder={this.state.inputValue} style={{ width: '250px',marginRight:'20px' }} />
-                    <Button  type="primary" >新增</Button>
-                </div>
-                <div style={{margin:'10px',width:'300px'}}>
-                    <List bordered dataSource={this.state.list} renderItem={item => (<List.Item>{item}</List.Item>)} />
-                </div>
-            </div> 
-         );
-    }
+  constructor(props) {
+    super(props);
+    console.log(store.getState());
+    this.state = store.getState();
+    this.changeInputValue = this.changeInputValue.bind(this);
+    this.storeChange = this.storeChange.bind(this);
+    store.subscribe(this.storeChange);
+  }
+  render() {
+    return (
+      <div>
+        <div style={{ margin: "10px" }}>
+          <Input
+            placeholder={this.state.inputValue}
+            style={{ width: "250px", marginRight: "20px" }}
+            onChange={this.changeInputValue}
+            value={this.state.inputValue}
+          />
+          <Button type="primary">新增</Button>
+        </div>
+        <div style={{ margin: "10px", width: "300px" }}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={item => <List.Item>{item}</List.Item>}
+          />
+        </div>
+      </div>
+    );
+  }
+  changeInputValue(e) {
+    const action = {
+      type: "changeInput",
+      value: e.target.value
+    };
+    store.dispatch(action);
+  }
+  storeChange() {
+    this.setState(store.getState());
+  }
 }
- 
+
 export default ToDoList;
--- react/demo4/src/store/reducer.js
+++ react/demo4/src/store/reducer.js
@@ -1,11 +1,14 @@
 const defaultState = {
-    inputValue: "请输入增加内容",
-    list: [
-        '早上八点上班',
-        '晚上八点下班'
-    ]
-}
+  inputValue: "请输入增加内容",
+  list: ["早上八点上班", "晚上八点下班"]
+};
 
-export default (state=defaultState, action) => {
-    return state
-}
+export default (state = defaultState, action) => {
+  //   Reducer里只能接受state,不能改变state
+  if (action.type === "changeInput") {
+    let newState = JSON.parse(JSON.stringify(state)); //深度拷贝state
+    newState.inputValue = action.value;
+    return newState;
+  }
+  return state;
+};
