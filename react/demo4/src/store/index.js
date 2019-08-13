3c3402a0180664ba30ee7938ca9c5200e1a64c3d
wangzhongjie <uvdream@163.com>
2019-08-13 14:41:34 +0800

    [fix] redux-thunk

--- react/demo4/src/store/index.js
+++ react/demo4/src/store/index.js
@@ -1,4 +1,18 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__());
+/*
+ * @Author: wangzhongjie
+ * @Date: 2019-08-07 09:35:09
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-08-13 14:41:22
+ * @Description:redux入口
+ * @Email: UvDream@163.com
+ */
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "./reducer";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  : compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(reducer, enhancer);
 export default store;
