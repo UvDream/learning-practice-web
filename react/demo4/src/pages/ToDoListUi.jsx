4b8ae5cdd23f279c64720b0c8498f95c45e20b00
wangzhongjie <uvdream@163.com>
2019-08-13 14:14:23 +0800

    [fix]bug修复

--- react/demo4/src/pages/ToDoListUi.jsx
+++ react/demo4/src/pages/ToDoListUi.jsx
@@ -22,7 +22,7 @@ class ToDoListUi extends Component {
             dataSource={this.props.list}
             renderItem={(item, index) => (
               <List.Item
-                onClick={index => {
+                onClick={() => {
                   this.props.deleteItem(index);
                 }}
               >
