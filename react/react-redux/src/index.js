8dc403bbd16ff600fd9d3e6f5f876b4ac71ac0b0
wangzhongjie <uvdream@163.com>
2019-08-13 15:15:19 +0800

    [feat] 初始化项目

---
+++ react/react-redux/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: wangzhongjie
+ * @Date: 2019-08-13 15:12:19
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-08-13 15:14:23
+ * @Description: 
+ * @Email: UvDream@163.com
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ToDoList from './pages/ToDoList'
+
+ReactDOM.render(<ToDoList />, document.getElementById('root'));
+
+
---
+++ react/react-redux/src/pages/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  <div>初始化</div>);
+    }
+}
+ 
+export default ToDoList;
