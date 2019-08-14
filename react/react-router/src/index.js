4d9a77e12c9e000d338dc00a901f291f3307231a
wangzhongjie <uvdream@163.com>
2019-08-14 10:30:36 +0800

    [fix]初始化

---
+++ react/react-router/src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: wangzhongjie
+ * @Date: 2019-08-14 10:11:18
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-08-14 10:22:05
+ * @Description: 
+ * @Email: UvDream@163.com
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
