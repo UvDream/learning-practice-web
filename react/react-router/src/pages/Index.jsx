fde15818691c12b885a4b602670084df1647a89e
wangzhongjie <uvdream@163.com>
2019-08-14 11:02:28 +0800

    [feat]列表传参

--- react/react-router/src/pages/Index.jsx
+++ react/react-router/src/pages/Index.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            list:[
+                {cid:1,title:'第1篇文章'},
+                {cid:2,title:'第2篇文章'},
+                {cid:3,title:'第3篇文章'}
+
+            ]
+         }
     }
     render() { 
-        return ( <div>首页</div> );
+        return (
+            <div>
+                <ul>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+
+                                <li key={index}>
+                                    <Link to={'/list/'+item.cid}>
+                                        {item.title}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        );
     }
 }
  
