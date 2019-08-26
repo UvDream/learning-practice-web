f376fc88f9d49b3c69a730941114c718552a2769
wangzhongjie <uvdream@163.com>
2019-08-26 21:03:20 +0800

    [feat] 比较复杂的reducer替代方案

--- react/react-hooks/src/pages/example/color.jsx
+++ react/react-hooks/src/pages/example/color.jsx
@@ -1,8 +1,18 @@
-import React,{createContext} from 'react';
-export const ColorContext=createContext({});
-export const Color=props=>{
-    return(
-        <ColorContext.Provider value={{color:"red"}}>
+import React, { createContext ,useReducer} from 'react';
+export const ColorContext = createContext({});
+export const UPDATE_COLOR = 'UPDATE_COLOR';
+const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_COLOR:
+            return action.color
+        default:
+            return state;
+    }
+}
+export const Color = props => {
+    const[color,dispatch]=useReducer(reducer,'blue');
+    return (
+        <ColorContext.Provider value={{ color,dispatch }}>
             {props.children}
         </ColorContext.Provider>
     )
