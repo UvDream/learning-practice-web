b1cc1c5fb9e212b1ecfb8edd361cc185094432a3
wangzhongjie <uvdream@163.com>
2019-08-26 18:21:19 +0800

    [fix] reducer

--- react/react-hooks/src/pages/App.jsx
+++ react/react-hooks/src/pages/App.jsx
@@ -1,24 +1,25 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Example from './example'
 import Exampled from './example3'
-import { BrowserRouter as Router,Route,Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Example4 from './example4'
+import '../index'
 // useEffect 离开生命周期
-function Index(){
-    useEffect(()=>{
+function Index() {
+    useEffect(() => {
         console.log(`useEffect,Index页面`)
-        return ()=>{
+        return () => {
             console.log("-----------离开Index页面")
         }
-    },[])
+    }, [])
     return (
         <h1>这是首页</h1>
     )
 }
-function List(){
-    useEffect(()=>{
+function List() {
+    useEffect(() => {
         console.log(`useEffect,List页面`)
-        return ()=>{
+        return () => {
             console.log("+++++++++++++离开列表页面")
         }
     })
@@ -27,36 +28,46 @@ function List(){
     )
 }
 function App() {
-     const [count, setCount] = useState(0); //数组解构
+    const [count, setCount] = useState(0); //数组解构
     //  生命周期函数
-     useEffect(()=>{
-         console.log(`生命周期函数useEffect${count}`)
-         return ()=>{
-             console.log('0000000000000000000')
-         }
-     },[count])
+    useEffect(() => {
+        console.log(`生命周期函数useEffect${count}`)
+        return () => {
+            console.log('0000000000000000000')
+        }
+    }, [count])
 
     return (
-            <div>
+        <div className="main">
+            <section>
                 你点击的次数{count}
-            <button onClick={() => { setCount(count + 1) }}>点击</button>
-            <Example />
-            <Exampled />
-            <Router>
-                <ul>
-                    <li>
-                        <Link to="">首页</Link>
-                    </li>
-                    <li>
-                        <Link to="/list/">列表</Link>
-                    </li>
-                </ul>
-                <Route path="/" exact component={Index}></Route>
-                <Route path="/"  component={List}></Route>
-            </Router>
-            <Example4/>
-            </div>
+                <button onClick={() => { setCount(count + 1) }}>点击</button>
+            </section>
+            <section>
+                <Example />
+            </section>
+            <section>
+                <Exampled />
+            </section>
+            <section>
+                <Router>
+                    <ul>
+                        <li>
+                            <Link to="">首页</Link>
+                        </li>
+                        <li>
+                            <Link to="/list/">列表</Link>
+                        </li>
+                    </ul>
+                    <Route path="/" exact component={Index}></Route>
+                    <Route path="/" component={List}></Route>
+                </Router>
+            </section>
+            <section>
+                <Example4 />
+            </section>
+        </div>
     )
 }
- 
+
 export default App;
