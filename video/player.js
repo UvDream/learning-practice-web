a65a475933e6f740ae906e5193e5894752a28dd4
wangzhongjie <UvDream@163.com>
2019-10-08 08:47:12 +0800

    [fix] player

--- video/player.js
+++ video/player.js
@@ -1,5 +1,12 @@
-function Player(options){
-    console.log(options);
-    this.$options=options || {}
-
+/*
+ * @Author: wangzhongjie
+ * @Date: 2019-09-10 17:34:50
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-10-18 08:46:44
+ * @Description:播放器
+ * @Email: UvDream@163.com
+ */
+function Player(options) {
+  console.log(options);
+  this.$options = options || {};
 }
