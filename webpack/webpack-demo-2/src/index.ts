8a457cc2d465ffc8c5458b36f269db2b3b609b4d
wangzhongjie <wangzj@cyberpeace.cn>
2021-07-12 15:29:39 +0800

    fix🐛:  修复ts类型错误

--- webpack/webpack-demo-2/src/index.ts
+++ webpack/webpack-demo-2/src/index.ts
@@ -1,1 +1,20 @@
-console.log("111");
+import "./index.less"
+import webpack from "./Webpack.png"
+class Test {
+    constructor() {
+      // document.write('hello world')
+      this.renderDiv()
+    }
+    renderDiv() {
+      const div = document.createElement('div')
+      div.className = 'test'
+      div.innerHTML = 'hello world'
+      document.body.appendChild(div)
+      const img = document.createElement('img')
+    img.src = webpack
+    document.body.appendChild(img)
+    }
+  
+  }
+  
+  new Test()
---
+++ webpack/webpack-demo-2/type.d.ts
@@ -0,0 +1,5 @@
+
+declare module '*.png' {
+
+    export default classes
+}
